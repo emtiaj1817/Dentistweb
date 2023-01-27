b647fb2939c44a7906c7060dc167a5ff32f808ac
Emtiaj jaman <100458152+emtiaj1817@users.noreply.github.com>
2023-01-27 22:39:59 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.testimonial-lab ul li').click(function(){
+        $(".testimonial-lab ul li").removeClass("active");
+        $(this).addClass("active");
+       
+     });
+    
+    
+     
+	
+});
